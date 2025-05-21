2f73e29d22bd3a32203389d5d0124dfedf0eac49
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-05-21 19:38:02 +0900

    feat: CoinAlert 컴포넌트 퍼블리싱

---
+++ src/assets/CloseIcon.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const CloseIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="icon-close">
+        <path
+          id="Vector"
+          d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18"
+          stroke="#585858"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default CloseIcon;
---
+++ src/components/CoinAlert.tsx
@@ -0,0 +1,48 @@
+import CloseIcon from "@/assets/CloseIcon";
+import React from "react";
+
+interface CoinStatusProps {
+  status: "매수" | "매도";
+}
+
+const CoinAlert = ({ status }: CoinStatusProps) => {
+  return (
+    <main className="flex flex-col w-[560px] items-center justify-center p-9 rounded-lg gap-8">
+      <div className="flex flex-col w-full gap-3">
+        <div className="flex justify-between w-full">
+          <span className="text-h3 text-grey-1000 font-[semibold]">
+            Coin {status}하기
+          </span>
+          <CloseIcon />
+        </div>
+        <div className="flex flex-col text-p2 text-grey-1000">
+          <span>현재 가지고 계신 자산은 100,000,000원 입니다.</span>
+          <span>
+            {status} 시 <span className="text-primary">0</span>원이 됩니다.
+          </span>
+        </div>
+      </div>
+
+      <input
+        type="number"
+        placeholder={`${status} 할 코인의 개수를 입력해 주십시오`}
+        className="w-full px-4 py-3 text-p1 border border-grey-500 rounded-lg focus:outline-none font-[regular] text-p2 text-grey-1300 placeholder:text-grey-600"
+      />
+
+      <div className="flex ml-auto gap-4 text-h5">
+        <button className="px-3 py-1 font-[semibold] text-grey-600 rounded-lg">
+          취소
+        </button>
+        <button
+          className={`px-3 py-1.5 font-[semibold] text-white rounded-lg ${
+            status === "매도" ? "bg-sell" : "bg-buy"
+          }`}
+        >
+          {status}하기
+        </button>
+      </div>
+    </main>
+  );
+};
+
+export default CoinAlert;
