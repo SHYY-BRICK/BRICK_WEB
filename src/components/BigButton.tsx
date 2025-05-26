8a9e2c3d29aaf1489041ea132472f7dce25d2589
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-05-26 15:56:31 +0900

    feat: BigButton 컴포넌트

---
+++ src/components/BigButton.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useState } from "react";
+
+interface BigButtonProps {
+  text: string;
+}
+
+const BigButton = ({ text }: BigButtonProps) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked((prev) => !prev);
+  };
+  return (
+    <button
+      onClick={handleClick}
+      className={`w-fit py-[0.5625rem] px-8 rounded-lg text-white text-h3 font-[semibold] hover:bg-hoverBtn ${
+        isClicked ? "bg-primary" : "bg-grey-600"
+      }`}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default BigButton;
