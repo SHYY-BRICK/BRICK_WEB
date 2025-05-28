e17c997913857e497e7ce5a3e3e1bf96d87aa2a8
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-05-28 14:09:52 +0900

    feat: 아이템데이터 선언

---
+++ src/data/accessories.ts
@@ -0,0 +1,45 @@
+import Drumstick from "@/assets/accessories/강시우의 드럼스틱.png";
+import ZeroCard from "@/assets/accessories/건우쌤의 제로카드.png";
+import PruneJuice from "@/assets/accessories/김시연의 푸룬주스.png";
+import Camcorder from "@/assets/accessories/김예진의 캠코더.png";
+import BloodSugarMedicine from "@/assets/accessories/누군가의 혈당약.png";
+import DarkCircle from "@/assets/accessories/다크서클.png";
+import Veil from "@/assets/accessories/면사포.png";
+import Scarf from "@/assets/accessories/목도리.png";
+import Perfume from "@/assets/accessories/박강원의 향수.png";
+import Beret from "@/assets/accessories/베레모.png";
+import Glasses from "@/assets/accessories/뿔테안경.png";
+import Sprout from "@/assets/accessories/새싹.png";
+import DevilAccessory from "@/assets/accessories/악마 장식.png";
+import AirPods from "@/assets/accessories/에어팟.png";
+import HygieneSet from "@/assets/accessories/위생세트.png";
+import Hat from "@/assets/accessories/정수환의 모자.png";
+import AngelAccessory from "@/assets/accessories/천사 장식.png";
+import Cap from "@/assets/accessories/캡모자.png";
+import Mustache from "@/assets/accessories/콧수염.png";
+import Capybara from "@/assets/accessories/카피바라.png";
+import MarioHat from "@/assets/accessories/한태영의 마리오 모자.png";
+
+export const accessoriesDate = [
+  { id: 1, name: "강시우의 드럼스틱", image: Drumstick },
+  { id: 2, name: "건우쌤의 제로카드", image: ZeroCard },
+  { id: 3, name: "김시연의 푸룬주스", image: PruneJuice },
+  { id: 4, name: "김예진의 캠코더", image: Camcorder },
+  { id: 5, name: "누군가의 혈당약", image: BloodSugarMedicine },
+  { id: 6, name: "다크서클", image: DarkCircle },
+  { id: 7, name: "면사포", image: Veil },
+  { id: 8, name: "목도리", image: Scarf },
+  { id: 9, name: "박강원의 향수", image: Perfume },
+  { id: 10, name: "베레모", image: Beret },
+  { id: 11, name: "뿔테안경", image: Glasses },
+  { id: 12, name: "새싹", image: Sprout },
+  { id: 13, name: "악마 장식", image: DevilAccessory },
+  { id: 14, name: "에어팟", image: AirPods },
+  { id: 15, name: "위생세트", image: HygieneSet },
+  { id: 16, name: "정수환의 모자", image: Hat },
+  { id: 17, name: "천사 장식", image: AngelAccessory },
+  { id: 18, name: "카피바라", image: Capybara },
+  { id: 19, name: "캡모자", image: Cap },
+  { id: 20, name: "콧수염", image: Mustache },
+  { id: 21, name: "한태영의 마리오 모자", image: MarioHat },
+];
---
+++ src/data/clothes.ts
@@ -0,0 +1,57 @@
+import MUS from "@/assets/clothes/강시우의 NUS 옷.png";
+import Devloper from "@/assets/clothes/개발자룩.png";
+import Adidas from "@/assets/clothes/건우쌤의 아디다스 세트.png";
+import SchoolJumper from "@/assets/clothes/과잠.png";
+import SchoolPants from "@/assets/clothes/교복 바지.png";
+import SchoolSkirt from "@/assets/clothes/교복 치마.png";
+import Apron from "@/assets/clothes/급식실 앞치마.png";
+import Default from "@/assets/clothes/기본 옷.png";
+import Poop from "@/assets/clothes/김시연의 똥 옷.png";
+import BubbleGum from "@/assets/clothes/김예진의 버블검 옷.png";
+import YellowWorkout from "@/assets/clothes/노랑 체육복.png";
+import ManToMan from "@/assets/clothes/맨투맨.png";
+import HealthRoomGown from "@/assets/clothes/보건실 가운.png";
+import RedWorkout from "@/assets/clothes/빨강 체육복.png";
+import Devil from "@/assets/clothes/악마옷.png";
+import ManOldSchoolUniform from "@/assets/clothes/옛날 남자 교복.png";
+import WomanOldSchoolUniform from "@/assets/clothes/옛날 여자 교복.png";
+import OnePiece from "@/assets/clothes/원피스.png";
+import WeddingDress from "@/assets/clothes/웨딩드레스.png";
+import NuldyJersey from "@/assets/clothes/이민준의 널디 져지.png";
+import POTCShortRobe from "@/assets/clothes/정수환의 POTC 군복.png";
+import Suit from "@/assets/clothes/정장.png";
+import angel from "@/assets/clothes/천사옷.png";
+import BlueWorkout from "@/assets/clothes/파랑 체육복.png";
+import Mario from "@/assets/clothes/한태영의 마리오 옷.png";
+import Hoodie from "@/assets/clothes/후드티.png";
+import WhiteTJeans from "@/assets/clothes/흰티 청바지.png";
+
+export const clothesData = [
+  { id: 1, name: "강시우의 NUS 옷", image: MUS },
+  { id: 2, name: "개발자룩", image: Devloper },
+  { id: 3, name: "건우쌤의 아디다스 세트", image: Adidas },
+  { id: 4, name: "학교점퍼", image: SchoolJumper },
+  { id: 5, name: "교복 바지", image: SchoolPants },
+  { id: 6, name: "교복 치마", image: SchoolSkirt },
+  { id: 7, name: "급식실 앞치마", image: Apron },
+  { id: 8, name: "기본 옷", image: Default },
+  { id: 9, name: "김시연의 똥 옷", image: Poop },
+  { id: 10, name: "김예진의 버블검 옷", image: BubbleGum },
+  { id: 11, name: "맨투맨", image: ManToMan },
+  { id: 12, name: "보건실 가운", image: HealthRoomGown },
+  { id: 13, name: "노랑 체육복", image: YellowWorkout },
+  { id: 14, name: "빨강 체육복", image: RedWorkout },
+  { id: 15, name: "악마옷", image: Devil },
+  { id: 16, name: "옛날 남자 교복", image: ManOldSchoolUniform },
+  { id: 17, name: "옛날 여자 교복", image: WomanOldSchoolUniform },
+  { id: 18, name: "원피스", image: OnePiece },
+  { id: 19, name: "웨딩드레스", image: WeddingDress },
+  { id: 20, name: "이민준의 널디 져지", image: NuldyJersey },
+  { id: 21, name: "정수환의 POTC 군복", image: POTCShortRobe },
+  { id: 22, name: "정장", image: Suit },
+  { id: 23, name: "천사옷", image: angel },
+  { id: 24, name: "파랑 체육복", image: BlueWorkout },
+  { id: 25, name: "한태영의 마리오 옷", image: Mario },
+  { id: 26, name: "후드티", image: Hoodie },
+  { id: 27, name: "흰티 청바지", image: WhiteTJeans },
+];
