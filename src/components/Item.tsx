104fea239699a2f7b45a19c02c934b783efcf026
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-05-28 14:10:12 +0900

    feat: Item 컴포넌트

---
+++ src/components/Item.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import React from "react";
+import { accessoriesDate } from "@/data/accessories";
+import { clothesData } from "@/data/clothes";
+
+interface ItemProps {
+  category: "clothes" | "accessory";
+  name: string;
+}
+
+const Item = ({ category, name }: ItemProps) => {
+  const data = category === "clothes" ? clothesData : accessoriesDate;
+  const matchedItem = data.find((item) => item.name === name);
+
+  if (!matchedItem) {
+    return <div>아이템을 찾을 수 없습니다.</div>;
+  }
+
+  return (
+    <main className="flex flex-col items-center justify-center w-[30vw] h-[51.67vh] gap-5 rounded-xl bg-white">
+      <Image
+        src={matchedItem.image}
+        alt={matchedItem.name}
+        height={160}
+        className="scale-[2] -translate-y-6 object-contain"
+      />
+      <span className="text-primary text-h4 font-[semibold]">
+        {matchedItem.name}
+      </span>
+    </main>
+  );
+};
+
+export default Item;
