d0949065ab049d2b0fe128b56beb149d13864c07
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-05-29 11:42:11 +0900

    style: width, height 단위 px로 변경

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -17,7 +17,7 @@ const Item = ({ category, name }: ItemProps) => {
   }
 
   return (
-    <main className="flex flex-col items-center justify-center w-[30vw] h-[51.67vh] gap-5 rounded-xl bg-white">
+    <main className="flex flex-col items-center justify-center w-[216px] h-[248px] gap-5 rounded-xl bg-white">
       <Image
         src={matchedItem.image}
         alt={matchedItem.name}
