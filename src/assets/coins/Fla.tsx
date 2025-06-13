26b2cc12514c795b2aa1c6471b664a045c4c3b63
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-06-13 17:17:17 +0900

    feat: MyTransaction 컴포넌트

---
+++ src/assets/coins/Fla.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Fla = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_1026_3083)">
+        <path
+          d="M8.31111 11.3516H2.77037V14.2751H8.14815V16.6267H2.77037V20.9802L0.684444 21.6952C0.228148 21.8541 0 21.7111 0 21.2662V9H8.31111V11.3516Z"
+          fill="#007BFF"
+        />
+        <path
+          d="M18.9018 21.4251H10.2648V9H13.0351V19.0736H18.9018V21.4251Z"
+          fill="#007BFF"
+        />
+        <path
+          d="M31.7739 21.4251H28.8895L27.8792 18.6763H23.2673L22.2732 21.4251H19.3888L24.0984 9H27.0643L31.7739 21.4251ZM25.5814 12.3367L24.131 16.3248H27.0318L25.5814 12.3367Z"
+          fill="#007BFF"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_1026_3083">
+          <rect
+            width="32"
+            height="13"
+            fill="white"
+            transform="translate(0 9)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default Fla;
---
+++ src/components/MyTransaction.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { coins } from "@/data/coins";
+import { ComponentType } from "react";
+
+type TransactionType = "매수" | "매도";
+
+interface MyTransactionProps {
+  type: TransactionType;
+  label: string;
+  amount: string;
+  date: string;
+  period: string;
+}
+
+const MyTransaction = ({
+  type,
+  label,
+  amount,
+  date,
+  period,
+}: MyTransactionProps) => {
+  const typeColor = type === "매수" ? "text-buy" : "text-sell";
+
+  const matchedCoin = coins.find((coin) => coin.label === label);
+  const Icon = matchedCoin?.image as ComponentType | null;
+
+  return (
+    <main className="flex justify-between w-[32.5rem] py-5 border-b border-grey-500">
+      <article className="flex items-center">
+        <p className={`text-h5 font-semibold pr-4 ${typeColor}`}>{type}</p>
+        <figure>
+          {Icon ? <Icon /> : <div className="bg-red-400 w-4 h-4" />}
+        </figure>
+        <p className="text-h5 text-grey-1000 font-semibold pl-3">{label}</p>
+      </article>
+      <article className="flex gap-[4.625rem] items-center">
+        <p className="text-grey-1100 text-h5 font-semibold">{amount}</p>
+        <figure className="flex text-p1 text-grey-1100 gap-4">
+          <p>{date}</p>
+          <p>{period}</p>
+        </figure>
+      </article>
+    </main>
+  );
+};
+
+export default MyTransaction;
