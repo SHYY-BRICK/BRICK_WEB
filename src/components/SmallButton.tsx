a42c5a9bb5f5d5cf4509923cb4860586745d365a
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-06-14 18:51:04 +0900

    feat: onClick 기능 추가

--- src/components/SmallButton.tsx
+++ src/components/SmallButton.tsx
@@ -5,9 +5,14 @@ import React from "react";
 interface SmallButtonProps {
   text: string;
   colorType: "primary" | "transparent" | "red";
+  onClick?: () => void;
 }
 
-const SmallButton = ({ text, colorType = "transparent" }: SmallButtonProps) => {
+const SmallButton = ({
+  text,
+  colorType = "transparent",
+  onClick,
+}: SmallButtonProps) => {
   const colorClasses = {
     primary: "bg-primary text-white",
     transparent: "bg-transparent text-grey-600",
@@ -16,6 +21,7 @@ const SmallButton = ({ text, colorType = "transparent" }: SmallButtonProps) => {
 
   return (
     <button
+      onClick={onClick}
       className={`w-fit py-1 px-3 rounded-lg text-p1 font-[regular] ${colorClasses[colorType]}`}
     >
       {text}
