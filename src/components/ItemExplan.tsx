6b3dd968ec973484260b4023906d0ff6147f5bb2
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-06-15 23:51:13 +0900

    feat: 스타일 수정 및 onClose props 추가

--- src/components/ItemExplan.tsx
+++ src/components/ItemExplan.tsx
@@ -8,9 +8,10 @@ interface ItemProps {
   category: "clothes" | "accessory";
   name: string;
   isMine?: boolean;
+  onClose?: () => void;
 }
 
-const ItemExplan = ({ category, name, isMine }: ItemProps) => {
+const ItemExplan = ({ category, name, isMine, onClose }: ItemProps) => {
   const data = category === "clothes" ? clothesData : accessoriesDate;
   const matchedItem = data.find((item) => item.name === name);
 
@@ -18,14 +19,14 @@ const ItemExplan = ({ category, name, isMine }: ItemProps) => {
     return <div>아이템을 찾을 수 없습니다.</div>;
   }
   return (
-    <main className="flex p-6 gap-[1.6rem] rounded-lg">
+    <main className="flex w-[25rem] h-[12.5rem] p-6 gap-[1.6rem] rounded-lg bg-white">
       <Image
         src={matchedItem.image}
         alt={matchedItem.name}
         height={120}
         className="scale-[2] -translate-y-6 object-contain"
       />
-      <section className="flex flex-col gap-[1.9rem] w-[12.8rem]">
+      <section className="flex flex-col justify-between w-full">
         <header className="flex flex-col w-full">
           <span className="text-h4 text-grey-1000">{matchedItem.name}</span>
           <span className="text-p2 text-grey-700">{matchedItem.info}</span>
@@ -37,7 +38,7 @@ const ItemExplan = ({ category, name, isMine }: ItemProps) => {
               <SmallButton text="해제" colorType="red" />
             </>
           ) : (
-            <SmallButton text="완료" colorType="red" />
+            <SmallButton text="완료" colorType="primary" onClick={onClose} />
           )}
         </figure>
       </section>
