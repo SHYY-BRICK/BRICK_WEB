01cfb8bc1f9bb63edfec07306e2b8bf34aad23f0
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-06-16 00:45:46 +0900

    feat: 이미지 확대 삭제 및 onClose 추가

--- src/components/ItemAlert.tsx
+++ src/components/ItemAlert.tsx
@@ -4,36 +4,37 @@ import { clothesData } from "@/data/clothes";
 import Image from "next/image";
 import React from "react";
 
-interface ItemProps {
+interface ItemAlertProps {
   category: "clothes" | "accessory";
   name: string;
+  onClose: () => void;
 }
 
-const ItemAlert = ({ category, name }: ItemProps) => {
+const ItemAlert = ({ category, name, onClose }: ItemAlertProps) => {
   const data = category === "clothes" ? clothesData : accessoriesDate;
   const matchedItem = data.find((item) => item.name === name);
 
   if (!matchedItem) {
     return <div>아이템을 찾을 수 없습니다.</div>;
   }
+
   return (
-    <main className="flex flex-col w-[300px] h-[376px] rounded-lg py-6 px-[2.125rem]">
+    <main className="flex flex-col w-[300px] h-[376px] bg-white rounded-lg py-6 px-[2.125rem] shadow-lg">
       <header className="flex justify-between items-center">
         <span className="text-h3 text-grey-1200 font-semibold">
           {matchedItem.name}
         </span>
-        <CloseIcon />
+        <button className="z-10" onClick={onClose}>
+          <CloseIcon />
+        </button>
       </header>
+
       <span className="text-p1 text-grey-800 font-regular pt-2 pb-6">
         {matchedItem.info}
       </span>
-      <div className="flex items-center justify-center">
-        <Image
-          src={matchedItem.image}
-          alt={matchedItem.name}
-          height={176}
-          className="scale-[2] -translate-y-6 object-contain"
-        />
+
+      <div className="flex items-center justify-center flex-1">
+        <Image src={matchedItem.image} alt={matchedItem.name} height={176} />
       </div>
     </main>
   );
