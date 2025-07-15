21b066877aef7277f0f7a6bfe0067faf0f9ebb7f
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-07-15 21:39:32 +0900

    style: height 지정

--- src/components/NewsSmall.tsx
+++ src/components/NewsSmall.tsx
@@ -8,7 +8,7 @@ interface NewsSmallProps {
 
 const NewsSmall = ({ title, description, date }: NewsSmallProps) => {
   return (
-    <main className="flex flex-col w-full rounded-xl p-6 gap-[0.5625rem] bg-white">
+    <main className="flex flex-col w-full h-[11.5625rem] rounded-xl p-6 gap-[0.5625rem] bg-white overflow-hidden">
       <p className="text-h5 text-grey-1200 font-semibold">{title}</p>
       <p className="text-p2 text-grey-700 font-regular">{description}</p>
       <p className="text-caption text-grey-900 font-medium">{date}</p>
