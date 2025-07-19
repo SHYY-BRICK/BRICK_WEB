8a36d2e283c1695051f112c93a9fbfd5a7db5f52
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-07-19 23:28:07 +0900

    fix: 로고 아이콘 추가 및 언어 설정 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,20 @@
 import { ReactQueryProvider } from "@/lib/react-query";
 import "./globals.css";
 
+export const metadata = {
+  title: "BRICK",
+  icons: {
+    icon: "/logo.png",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body>
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
